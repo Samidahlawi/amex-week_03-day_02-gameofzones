0f709272abd4722b1ec1b3e3a5a9a96ee213f1cb
Moath <mfalthaw@ucalgary.ca>
2019-01-28 10:43:09 +0300

    gameofzones-1 done

--- starter1.js
+++ starter1.js
@@ -1,3 +1,28 @@
-console.log('starter1.js linked');
+let clickCounter = 0;
 
-//code goes here
+function makeZoneGreen(event) {
+  console.log("mouse entered!");
+  $(event.target).css("background-color", "green");
+}
+
+function makeZoneGreenForEver(event) {
+  console.log("zone clicked!");
+  $(event.target).css("background-color", "green");
+  $(event.target).off("mouseenter");
+  $(event.target).off("click");
+  $(event.target).off("mouseleave");
+
+  clickCounter++;
+  if (clickCounter === 4) {
+    console.log("Congratulations!");
+  }
+}
+
+function makeZoneWhite(event) {
+  console.log("mouse left!");
+  $(event.target).css("background-color", "white");
+}
+
+$(".zone").on("mouseenter", makeZoneGreen);
+$(".zone").on("mouseleave", makeZoneWhite);
+$(".zone").on("click", makeZoneGreenForEver);
